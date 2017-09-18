8d353a5e66dad3bfbf3c7afd3703fd82102ab8f8
Klevison Matias <klevison@gmail.com>
2017-09-18 19:34:56 -0300

    Dependencies version updated

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@ let package = Package(
     name: "RethinkDBSwift",
     dependencies: [
         // .Package(url: "https://github.com/IBM-Swift/BlueSocket", Version(0, 12, 39)),
-        .Package(url: "https://github.com/IBM-Swift/BlueSSLService", Version(0, 12, 30)),
-        .Package(url: "https://github.com/IBM-Swift/BlueCryptor", Version(0, 8, 8)),
+        .Package(url: "https://github.com/IBM-Swift/BlueSSLService", majorVersion: 0),
+        .Package(url: "https://github.com/IBM-Swift/BlueCryptor", majorVersion: 0),
         .Package(url: "https://github.com/jjacobson93/WarpCore", majorVersion: 0)
     ]
 )
